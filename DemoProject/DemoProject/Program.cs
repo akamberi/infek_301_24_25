4894f919f837a022995a17875597a9fc1b4eac25
Afrim Kamberi <afrimkamberi2@gmail.com>
2024-10-07 09:26:55 +0200

    Update Program.cs

--- DemoProject/DemoProject/Program.cs
+++ DemoProject/DemoProject/Program.cs
@@ -3,6 +3,7 @@
     public static void Main(string[] args)
     {
         Console.WriteLine("Hey there, this is my first app");
+        Console.WriteLine("This is my second line on this first app");
         Console.ReadLine();
     }
 }
